fdd9f9f26d1f96b45358386c8509accc6db06cfc
Palak Kumrawat <100283614+palak-k5@users.noreply.github.com>
2023-12-25 23:52:56 +0530

    Add files via upload

---
+++ q1.1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  5 22:55:55 2023
+
+@author: hp
+"""
+
+# This is a comment!
+print("hello world")
+print("this is python programming")
+import pandas as pd
+import numpy as np
---
+++ q1.3.py
@@ -0,0 +1,6 @@
+
+
+import pandas as pd
+import numpy as np
+bank_train=pd.read_csv(r'C:\DSPR_Data_Sets\Website Data Sets\bank_marketing_training')
+print(bank_train)
